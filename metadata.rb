113acf7a1e3257ae8918530da04eff115cd09ad4
Jeff Byrnes <jeff@evertrue.com>
2013-12-10 13:35:16 -0500

    Bump to 2.0.4

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Webtrends, Inc."
 maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs Twitter's Storm distributed computation system"
-version          "2.0.3"
+version          "2.0.4"
 
 depends          "git"
 depends          "java"
