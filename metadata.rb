28d59704ebe119d7bdbfd618169a11b314d652d1
Eric Herot <eric.github@herot.com>
2014-05-01 14:45:16 -0400

    Linting cleanup

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
 name             'storm'
-maintainer       "Webtrends, Inc."
-maintainer_email "kendrick.martin@webtrends.com"
-license          "All rights reserved"
-description      "Installs Twitter's Storm distributed computation system"
-version          "2.0.5"
+maintainer       'Webtrends, Inc.'
+maintainer_email 'kendrick.martin@webtrends.com'
+license          'All rights reserved'
+description      'Installs Twitter\'s Storm distributed computation system'
+version          '2.0.5'
 
-depends          "git"
-depends          "java"
-depends          "runit"
-depends          "et_jzmq", '= 1.0.1'
+depends          'git'
+depends          'java'
+depends          'runit'
+depends          'et_jzmq', '= 1.0.1'
 
-supports         "ubuntu"
+supports         'ubuntu'
